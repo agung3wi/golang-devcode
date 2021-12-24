0844c74ccae066ec5add28bb65a385dd3d946953
Agung <agungskripsi05@gmail.com>
2021-12-24 22:19:17 +0700

    fixing sementara

--- main.go
+++ main.go
@@ -4,11 +4,28 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"unicode"
 )
 
 type Activity struct {
-	Email string `json:"email"`
-	Title string `json:"title"`
+	ID        int         `json:"id"`
+	Email     string      `json:"email"`
+	Title     string      `json:"title"`
+	CreatedAt string      `json:"created_at"`
+	UpdatedAt string      `json:"updated_at"`
+	DeletedAt interface{} `json:"deleted_at"`
+}
+
+type Todo struct {
+	ID              int         `json:"id"`
+	ActivityGroupId string      `json:"activity_group_id"`
+	Title           string      `json:"title"`
+	IsActive        interface{} `json:"is_active"`
+	Priority        string      `json:"priority"`
+	CreatedAt       string      `json:"created_at"`
+	UpdatedAt       string      `json:"updated_at"`
+	DeletedAt       interface{} `json:"deleted_at"`
 }
 
 type response struct {
@@ -21,11 +38,15 @@ type Kosong struct {
 }
 
 var kosong Kosong
+var activities = []Activity{}
+var todos = []Todo{}
 
 func main() {
+	// r := mux.NewRouter()
 	http.HandleFunc("/", HelloServer)
 	http.HandleFunc("/activity-groups", ActivityRest)
-	http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/todo-items", TodoRest)
+	http.ListenAndServe(":3030", nil)
 }
 
 func ActivityRest(w http.ResponseWriter, r *http.Request) {
@@ -35,6 +56,10 @@ func ActivityRest(w http.ResponseWriter, r *http.Request) {
 		decoder := json.NewDecoder(r.Body)
 		var t Activity
 		err := decoder.Decode(&t)
+		if err != nil {
+			fmt.Fprint(w, "Test Error")
+			return
+		}
 
 		if t.Title == "" {
 			var resp response
@@ -52,28 +77,321 @@ func ActivityRest(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		t.CreatedAt = "2021-12-01T09:23:05.825Z"
+		t.UpdatedAt = "2021-12-01T09:23:05.825Z"
+		t.DeletedAt = nil
+		t.ID = len(activities) + 1
+		activities = append(activities, t)
+
+		var resp response
+		resp.Status = "Success"
+		resp.Message = "Success"
+		resp.Data = t
+		jData, err := json.Marshal(resp)
 		if err != nil {
-			fmt.Fprint(w, "Internal Server Error")
-		} else {
-			fmt.Fprint(w, "Internal Server Error")
+			fmt.Fprint(w, "Test Error")
+			return
 		}
+		w.Write(jData)
+
 	case "GET":
-		w.Write([]byte("get"))
+		var resp response
+		resp.Status = "Success"
+		resp.Message = "Success"
+		resp.Data = activities
+		jData, err := json.Marshal(resp)
+		if err != nil {
+			fmt.Fprint(w, "Test Error")
+			return
+		}
+		w.Write(jData)
+
 	default:
 		http.Error(w, "", http.StatusBadRequest)
 	}
 
 }
 
+func TodoRest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	switch r.Method {
+	case "POST":
+		decoder := json.NewDecoder(r.Body)
+		var t Todo
+		err := decoder.Decode(&t)
+		if err != nil {
+			fmt.Fprint(w, "Test Error")
+			return
+		}
+
+		if t.Title == "" {
+			var resp response
+			resp.Status = "Bad Request"
+			resp.Message = "title cannot be null"
+			resp.Data = kosong
+			w.WriteHeader(http.StatusBadRequest)
+
+			jData, err := json.Marshal(resp)
+			if err != nil {
+				fmt.Fprint(w, "Test Error")
+				return
+			}
+			w.Write(jData)
+			return
+		}
+
+		if t.ActivityGroupId == "" {
+			var resp response
+			resp.Status = "Bad Request"
+			resp.Message = "activity_group_id cannot be null"
+			resp.Data = kosong
+			w.WriteHeader(http.StatusBadRequest)
+
+			jData, err := json.Marshal(resp)
+			if err != nil {
+				fmt.Fprint(w, "Test Error")
+				return
+			}
+			w.Write(jData)
+			return
+		}
+
+		t.IsActive = "1"
+		t.CreatedAt = "2021-12-01T09:23:05.825Z"
+		t.UpdatedAt = "2021-12-01T09:23:05.825Z"
+		t.DeletedAt = nil
+		t.ID = len(todos) + 1
+		todos = append(todos, t)
+
+		var resp response
+		resp.Status = "Success"
+		resp.Message = "Success"
+		resp.Data = t
+		jData, err := json.Marshal(resp)
+		if err != nil {
+			fmt.Fprint(w, "Test Error")
+			return
+		}
+		w.Write(jData)
+
+	case "GET":
+		var resp response
+		resp.Status = "Success"
+		resp.Message = "Success"
+		resp.Data = todos
+		jData, err := json.Marshal(resp)
+		if err != nil {
+			fmt.Fprint(w, "Test Error")
+			return
+		}
+		w.Write(jData)
+
+	default:
+		http.Error(w, "", http.StatusBadRequest)
+	}
+
+}
+
+func isInt(s string) bool {
+	for _, c := range s {
+		if !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func HandleParamActivity(w http.ResponseWriter, r *http.Request, ids string) {
+	var resp response
+	id, err := strconv.Atoi(ids)
+	if err != nil {
+		return
+	}
+
+	if id > len(activities) {
+		resp.Status = "Not Found"
+		resp.Message = "Activity with ID " + ids + " Not Found"
+		resp.Data = kosong
+		jData, err := json.Marshal(resp)
+		if err != nil {
+			fmt.Fprint(w, "Test Error")
+			return
+		}
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(jData)
+
+	} else {
+
+		if r.Method == "GET" {
+			resp.Status = "Success"
+			resp.Message = "Success"
+			resp.Data = activities[id-1]
+			jData, err := json.Marshal(resp)
+			if err != nil {
+				fmt.Fprint(w, "Test Error")
+				return
+			}
+			w.Write(jData)
+			return
+		} else if r.Method == "PATCH" {
+			decoder := json.NewDecoder(r.Body)
+			var t Activity
+			err := decoder.Decode(&t)
+			if err != nil {
+				fmt.Fprint(w, "Test Error")
+				return
+			}
+
+			if t.Title == "" {
+				var resp response
+				resp.Status = "Bad Request"
+				resp.Message = "title cannot be null"
+				resp.Data = kosong
+				w.WriteHeader(http.StatusBadRequest)
+
+				jData, err := json.Marshal(resp)
+				if err != nil {
+					fmt.Fprint(w, "Test Error")
+					return
+				}
+				w.Write(jData)
+				return
+			}
+
+			activities[id-1].Title = t.Title
+			if t.Email != "" {
+				activities[id-1].Email = t.Email
+			}
+
+			resp.Status = "Success"
+			resp.Message = "Success"
+			resp.Data = activities[id-1]
+			jData, err := json.Marshal(resp)
+			if err != nil {
+				fmt.Fprint(w, "Test Error")
+				return
+			}
+			w.Write(jData)
+			return
+
+		} else if r.Method == "DELETE" {
+			resp.Status = "Success"
+			resp.Message = "Success"
+			activities[id-1].DeletedAt = nil
+			resp.Data = kosong
+			jData, err := json.Marshal(resp)
+			if err != nil {
+				fmt.Fprint(w, "Test Error")
+				return
+			}
+			w.Write(jData)
+			return
+
+		}
+	}
+	return
+}
+
+func HandleParamTodo(w http.ResponseWriter, r *http.Request, ids string) {
+	var resp response
+	id, err := strconv.Atoi(ids)
+	if err != nil {
+		return
+	}
+
+	if id > len(todos) {
+		resp.Status = "Not Found"
+		resp.Message = "Todo with ID " + ids + " Not Found"
+		resp.Data = kosong
+		jData, err := json.Marshal(resp)
+		if err != nil {
+			fmt.Fprint(w, "Test Error")
+			return
+		}
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(jData)
+
+	} else {
+
+		if r.Method == "GET" {
+			resp.Status = "Success"
+			resp.Message = "Success"
+			resp.Data = todos[id-1]
+			jData, err := json.Marshal(resp)
+			if err != nil {
+				fmt.Fprint(w, "Test Error")
+				return
+			}
+			w.Write(jData)
+			return
+		} else if r.Method == "PATCH" {
+			decoder := json.NewDecoder(r.Body)
+			var t Todo
+			err := decoder.Decode(&t)
+			if err != nil {
+				fmt.Fprint(w, "Test Error")
+				return
+			}
+
+			if t.Title != "" {
+				todos[id-1].Title = t.Title
+			}
+
+			if t.IsActive != "" {
+				todos[id-1].IsActive = t.IsActive
+			}
+
+			resp.Status = "Success"
+			resp.Message = "Success"
+			resp.Data = todos[id-1]
+			jData, err := json.Marshal(resp)
+			if err != nil {
+				fmt.Fprint(w, "Test Error")
+				return
+			}
+			w.Write(jData)
+			return
+
+		} else if r.Method == "DELETE" {
+			resp.Status = "Success"
+			resp.Message = "Success"
+			activities[id-1].DeletedAt = nil
+			resp.Data = kosong
+			jData, err := json.Marshal(resp)
+			if err != nil {
+				fmt.Fprint(w, "Test Error")
+				return
+			}
+			w.Write(jData)
+			return
+
+		}
+	}
+	return
+}
+
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	var names [4]string
-	names[0] = "trafalgar"
-	names[1] = "d"
-	names[2] = "water"
-	names[3] = "law"
+	var path = r.URL.Path
+	lenPath := len(path)
+	if lenPath > 17 {
+		if path[0:17] == "/activity-groups/" && isInt(path[17:lenPath]) {
+			ids := path[17:lenPath]
+			HandleParamActivity(w, r, ids)
+			return
+		}
+	}
 
+	if lenPath > 12 {
+		if path[0:12] == "/todo-items/" && isInt(path[12:lenPath]) {
+			ids := path[12:lenPath]
+			HandleParamTodo(w, r, ids)
+			return
+		}
+	}
+
+	names := "Oke"
 	jData, err := json.Marshal(names)
 	if err != nil {
 		fmt.Fprint(w, "Internal Server Error")
